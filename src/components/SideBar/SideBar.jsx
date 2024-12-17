1166d7bde637f0ca2525b040de7484b3799c07c7
kestsaurav21 <kestsaurav21@outlook.com>
2024-12-17 23:01:59 +0530

    fix: Sidbar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './SideBar.css'
 import NotesPanel from '../NotePanel/NotesPanel'
-const SideBar = ({ openModal, notes, openNote, setShowNote }) => {
+const SideBar = ({ openModal, notes, openNote, closeNote }) => {
   return (
     <div className="sidebar">
-        <h1 className="title" onClick={() => setShowNote(false)}>Pocket Notes</h1>
+        <h1 className="title" onClick={closeNote}>Pocket Notes</h1>
         <div className="notes-list">
          <NotesPanel notes={notes} openNote={openNote} />
         </div>
